2a283898ff159ded31f4fb9e13b4c615632653a7
Subh211 <145745637+Subh211@users.noreply.github.com>
2024-12-30 22:00:10 +0530

    new code added

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ app.use('/api', ApiRouter); // if any req comes with url starting with /api
 
 app.get('/ping', (req, res) => {
     return res.json({
-        message: 'subhadeep is the biggest hero'
+        message: 'subhadeep is the biggest hero and also very much alone'
     })
     //res.send('pongggggggggggggg')
 });
